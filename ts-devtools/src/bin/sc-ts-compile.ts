b77c888af9e7614be53b87dea41a66bd71745d1f
Sebastián Gurín <sgurin@awalabs.com>
2018-12-24 13:48:25 -0300

    sc-tsc

--- ts-devtools/src/bin/sc-ts-compile.ts
+++
@@ -1,8 +0,0 @@
-// require('minimist')(process.argv.slice(2));
-
-import minimist from 'minimist' 
-
-const args = minimist((process.argv.slice(2)))
-
-const config=args
-console.log(args);
---
+++ ts-devtools/src/bin/sc-tsc.ts
@@ -0,0 +1,38 @@
+import minimist from 'minimist'
+import { AllConfig, import2defineCompileAndFix } from '../util/import2defineCompileAndFix';
+
+function main() {
+  const args = minimist((process.argv.slice(2)))
+  const config: AllConfig = args as any
+  if (!checkRequiredParams(config)) {
+    exit('Invalid call, insufficient arguments. ', 1, true)
+  }
+  const result = import2defineCompileAndFix(config)
+  if (result.errors.length) {
+    exit(`There where errors: 
+${result.errors.map(e => e).join('\n')}`, 1)
+  }
+  else {
+    exit(`Sucessfully generated JS project "${config.outputFolder}"`, 0)
+  }
+}
+
+function checkRequiredParams(config: AllConfig) {
+  return config.tsconfigFilePath && config.outputFolder
+}
+
+function exit(msg: string, code: number = 0, showHelp = false) {
+  const log = code === 0 ? console.log : console.error
+  log(msg);
+  if (showHelp) {
+    console.log(help());
+  }
+  process.exit(code)
+}
+
+function help() {
+  return `
+Usage: sc-tsc --tsconfigFilePath my/ts/project/tsconfig.json --outputFolder my/compiled/project
+
+  `
+}
